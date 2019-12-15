e2ccf0a1262ef3ad41d1601d5be6fd44ee20d0bf
Rey Polanco <reypolanco@gmail.com>
2019-12-14 21:02:47 -0500

    add some breakpoint to support responsiveness on most devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const style = {
 
 const Header = () => (
 	<header
-		className="navbar navbar-expand-lg navbar-light mb-5 text-center d-flex justify-content-center"
+		className="navbar navbar-expand-lg navbar-light mb-3 text-center d-flex justify-content-center"
 		style={style}>
 		<h4 className="text-white mb-0">Giphy Explorer</h4>
 	</header>
